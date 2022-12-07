69282dd9de43fc060f96084a1746bdb085a77214
Johan Lång <johan.lang@vaxjo.se>
2022-12-07 17:00:06 +0100

    First try

--- 2022/Solution.cs
+++ 2022/Solution.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Day7
 {
@@ -6,7 +9,53 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> listA = new List<string>();
+            using (var reader = new StreamReader(@"C:\Users\johlan002\source\repos\Day7\Day7\input_test.in"))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+
+                    listA.Add(line);
+
+                }
+            }
+
+
+
+            foreach (var item in listA)
+            {
+                if (item.StartsWith("$ cd .."))
+                {
+                    Console.WriteLine("Going back one dir");
+                }
+                else if (item.StartsWith("$ cd "))
+                {
+                    Console.WriteLine("CD to " + item.Substring(5));
+                }
+                else if (item.StartsWith("$ ls"))
+                {
+                    Console.WriteLine("Listing content");
+                }
+                else if (item.StartsWith("dir"))
+                {
+                    Console.WriteLine(item);
+                }
+                else if (Regex.IsMatch(item.Substring(0, 1), @"^\d+$"))
+                {
+                    Console.WriteLine(item);
+                }
+                // Console.WriteLine(item);
+            }
+
+
         }
+
+
+
+
     }
 }
+
